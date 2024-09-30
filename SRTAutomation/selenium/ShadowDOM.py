1ff6c1c19300b6d16c5a1800092971a4da46d6b1
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-09-30 16:12:09 +0600

    2024 09 30

--- SRTAutomation/selenium/ShadowDOM.py
+++ SRTAutomation/selenium/ShadowDOM.py
@@ -4,6 +4,8 @@ from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import time
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #Install driver
 driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
@@ -48,7 +50,7 @@ driver.find_element(By.XPATH, '(//fluent-option[@value="2022-2024"])[1]').click(
 time.sleep(3)
 
 #Open the Section 1: Program Overview
-program_overview_shadow_host = driver.find_element(By.XPATH,'(//fluent-accordion-item[@class="rounded"])[1]')
+program_overview_shadow_host = driver.find_element(By.XPATH,'(//fluent-accordion-item[@class="rounded"])[2]')
 time.sleep(2)
 program_overview_shadow_root = driver.execute_script('return arguments[0].shadowRoot',program_overview_shadow_host)
 time.sleep(2)
@@ -105,15 +107,65 @@ time.sleep(3)
 #Add Pernership Strategy
 driver.find_element(By.XPATH, '(//fluent-button[@type=\'button\'][normalize-space()=\'Add New\'])[1]').click()
 time.sleep(2)
-# Locate the drop-down element
-dropdown = driver.find_element(By.XPATH, "(//fluent-select[@class=\"valid w-full collapsible outline required\"])[1]")
 
-# Create a Select object
-select = Select(dropdown)
+# # Locate the drop-down element
+# dropdown = driver.find_element(By.XPATH, "(//fluent-select[@class=\"valid w-full collapsible outline required\"])[1]")
+#
+# # Create a Select object
+# select = Select(dropdown)
+#
+# # Select an option by visible text
+# select.select_by_visible_text("Strategic (i.e. alliances, government and civil society networks)")
 
-# Select an option by visible text
-select.select_by_visible_text("Strategic (i.e. alliances, government and civil society networks)")
+# driver.find_element(By.XPATH, "(//fluent-select[@class=\"valid w-full collapsible outline required\"])[1]").click()
+# time.sleep(2)
 
+# identify_partnership_category_shadow_host = driver.find_element(By.XPATH, "(//fluent-select[@class=\"valid w-full collapsible outline required\"])[1]")
+# identify_partnership_category_shadow_root = driver.execute_script('return arguments[0].shadowRoot', identify_partnership_category_shadow_host)
+# identify_partnership_category_shadow_root.find_element(By.CSS_SELECTOR, 'div.control').click()
+
+# driver.find_element(By.XPATH, '(//div[@role="listbox"])[1]').click()
+
+identify_partnership_category_shadow_host = driver.find_element(By.XPATH, '(//fluent-select[@role="combobox"])[1]')
+identify_partnership_category_shadow_root = driver.execute_script('return arguments[0].shadowRoot', identify_partnership_category_shadow_host)
+# identify_partnership_category_shadow_root.find_element(By.CSS_SELECTOR, 'div.listbox')
+
+element = identify_partnership_category_shadow_root.find_element(By.CSS_SELECTOR, 'div.listbox')
+driver.execute_script("arguments.click();", element)
+
+
+# # Wait for the fluent-select element to be present
+# fluent_select = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.XPATH, '(//fluent-select[@class=\"valid w-full collapsible outline required\"])[1]'))
+# )
+#
+# # Find all fluent-option elements and select the desired one
+# options = fluent_select.find_elements(By.TAG_NAME, 'fluent-option')
+# for option in options:
+#     if option.text == 'Strategic (i.e. alliances, government and civil society networks)':
+#         option.click()
+#         break
+
+# # Wait for the fluent-select element to be clickable and click it
+# fluent_select = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.XPATH, '(//fluent-select[@class=\"valid w-full collapsible outline required\"])[1]')) #fluent-dialog, fluent-select
+# )
+# fluent_select.click()
+
+# # Find the desired option (e.g., by text or value)
+# # Options might be in a child element like a list (e.g., fluent-option)
+# desired_option = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.XPATH, "(//fluent-option[@value=\"1\"])[1]"))
+# )
+#
+# # Click the desired option
+# desired_option.click()
+
+# driver.find_element(By.CSS_SELECTOR, 'div.listbox').click()
+
+share_name_of_partner_shadow_host = driver.find_element(By.XPATH, '(//fluent-text-field[@class="valid outline required"])[1]')
+share_name_of_partner_shadow_root = driver.execute_script('return arguments[0].shadowRoot', share_name_of_partner_shadow_host)
+share_name_of_partner_shadow_root.find_element(By.CSS_SELECTOR, 'input.control').send_keys('test')
 
 time.sleep(10)
 driver.close()
